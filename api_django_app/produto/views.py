5506b3feed7fd53c734a38d9653d9206aa033dae
Diego Sarmanho <68619825+sarmanhodev@users.noreply.github.com>
2024-04-24 14:56:29 -0300

    Update views.py

--- api_django_app/produto/views.py
+++ api_django_app/produto/views.py
@@ -5,27 +5,30 @@ from .models import Produto
 from .serializers import ProdutoSerializer
 
 @api_view(['GET'])
-def listar_produtos(request):
+def listar_produtos(request): #LISTAR TODOS OS PRODUTOS
     produtos = Produto.objects.all()
     serializer = ProdutoSerializer(produtos, many=True)
     return Response(serializer.data)
 
+
 @api_view(['GET'])
-def detalhes_produto(request, pk):
+def detalhes_produto(request, pk): #LISTAR UM PRODUTO EM ESPECÍFICO
     produto = get_object_or_404(Produto, pk=pk)
     serializer = ProdutoSerializer(produto)
     return Response(serializer.data)
 
+
 @api_view(['POST'])
-def criar_produto(request):
+def criar_produto(request): #CRIA UM NOVO PRODUTO
     serializer = ProdutoSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=201)
     return Response(serializer.errors, status=400)
 
+
 @api_view(['PUT'])
-def atualizar_produto(request, pk):
+def atualizar_produto(request, pk): #ATUALIZA UM REGISTRO
     produto = get_object_or_404(Produto, pk=pk)
     serializer = ProdutoSerializer(produto, data=request.data)
     if serializer.is_valid():
@@ -33,8 +36,9 @@ def atualizar_produto(request, pk):
         return Response(serializer.data)
     return Response(serializer.errors, status=400)
 
+
 @api_view(['DELETE'])
-def excluir_produto(request, pk):
+def excluir_produto(request, pk): #DELETA UM PRODUTO ESPECÍFICO
     produto = get_object_or_404(Produto, pk=pk)
     produto.delete()
     return Response(status=204)
